8c645697388c7d454373d7efa0b400a370d9eba8
niha-2 <gen.3polym.hn@gmail.com>
2023-06-27 02:10:09 +0900

    [add] Rubyアウトプット課題（グループ分け問題）を追加

---
+++ ruby/random.rb
@@ -0,0 +1,23 @@
+# frozen_string_literal: true
+
+# == Group
+# @param [Array] members
+class Group
+  def initialize(members)
+    @members = members
+  end
+
+  # == random_grouping
+  # グループを3人と3人、または2人と4人にランダムに分ける
+  # @return [Array] members
+  def random_grouping
+    number_of_people = [3, 4].sample
+    @members.shuffle.each_slice(number_of_people) do |member|
+      p member
+    end
+  end
+end
+
+members = %w[A B C D E F]
+group = Group.new(members)
+group.random_grouping
